2fb6009ce22f8a75db4e3e065cceafe6086b729f
David Pasdar <davood.pasdar@ecube.de>
2022-02-04 08:54:31 +0100

    Initial Commit

---
+++ extension.js
@@ -0,0 +1,35 @@
+const Toolpackage = require('chioro-toolbox/toolpackage')
+const base = require('chioro-toolbox/toolbase')
+
+// Toolbox extensions are organized as "Tool Packages".
+// A ToolPackage is a collection of tool descriptions, including tests.
+// Exporting a ToolPackage object makes a JS module a toolbox extension (see last line)
+// base refers to the standard chioro library and can be used anywhere in the code with
+// the following syntax: base.nameOfTheStandardFunction
+
+const tools = new Toolpackage("My great toolbox extension")
+
+
+function niceFunction(input1, input2) {
+    return base.upperCaseText(input1) + " " + base.lowerCaseText(input2);
+}
+tools.add({
+    impl: niceFunction,
+    aliases: {
+        en: "niceFunction",
+        de: "netteFunktion"
+    },
+    args: {
+        en: ["input1", "input2"],
+        de: ["eingabe1", "eingabe2"]
+    },
+    tags: ["tag1", "tag2"],
+    tests: () => {
+        tools.expect(niceFunction("hello", "world")).toBe('HELLO world');
+        tools.expect(niceFunction("Helping", "World")).toBe('HELPING world');
+    }
+})
+
+
+//-------------WRITE YOUR FUNCTIONS ABOVE THIS LINE------------------
+tools.exportAll(exports)
---
+++ test.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const mod = require('./extension')
+mod.tools.testAllTools()
+mod.tools.stats()
